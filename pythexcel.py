09f19a0faba20ced60abf126ea70099ee24cde3c
Anant prasad <Anantprsd5@gmail.com>
2016-08-07 23:52:53 +0530

    pythexcel.py

--- pythexcel.py
+++ pythexcel.py
@@ -1,5 +1,5 @@
 from pyexcel_ods import get_data
-data=get_data("/home/anant/Desktop/Untitled 1.ods")
+data=get_data("/home/anant/Desktop/foss/pyth.ods")
 import json
 k=json.dumps(data)
 sum=int(k[13])+int(k[16])
